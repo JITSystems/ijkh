a565e5e225669f91f41b4e1454ea90effee67f23
lessthancold <lessthancold@gmail.com>
2013-03-14 11:32:31 +0400

    Error_Fix: Added user existance check

--- app/controllers/place_controller.rb
+++ app/controllers/place_controller.rb
@@ -1,53 +1,56 @@
 class PlaceController < ApplicationController
 	def index
 		@user = User.where(authentication_token: params[:auth_token]).first
-		@places = @user.places.select("id, title, city, street, building, apartment").where(is_active: true).includes(services: {tariff: {tariff_template: {field_templates: [:values, :field_template_list_values, :reading_field_template]}}})
-		@places =  @places.as_json(
-				include: 
-					{ services: 
-	                    { include: 
-	                    	{ tariff: 
-								{ include: 
-									{ tariff_template: 
-										{ include: 
-											{ field_templates: 
-												{include: 
-													[{values: {only: [:value, :tariff_id]}},
-													 {field_template_list_values: {only: [:id, :value]}},
-													 {reading_field_template: {only: [:id, :title]}}], only: [:id, :title]
-												}
-											}, only: [:id, :current_value, :title]
-										}
-									}, only: [:title, :id, :owner_type]
-								} 
-							}, only: [:title, :id]
-						}
-					}, only: [:title, :id] )
+		if @user do 
+			@places = @user.places.select("id, title, city, street, building, apartment").where(is_active: true).includes(services: {tariff: {tariff_template: {field_templates: [:values, :field_template_list_values, :reading_field_template]}}})
+			@places =  @places.as_json(
+					include: 
+						{ services: 
+		                    { include: 
+		                    	{ tariff: 
+									{ include: 
+										{ tariff_template: 
+											{ include: 
+												{ field_templates: 
+													{include: 
+														[{values: {only: [:value, :tariff_id]}},
+														 {field_template_list_values: {only: [:id, :value]}},
+														 {reading_field_template: {only: [:id, :title]}}], only: [:id, :title]
+													}
+												}, only: [:id, :current_value, :title]
+											}
+										}, only: [:title, :id, :owner_type]
+									} 
+								}, only: [:title, :id]
+							}
+						}, only: [:title, :id] )
+			
+			@places.each do |place|
+				@services = place[:services]
+				place.delete(:services)
+
+				@services.each do |service|
+					@field_templates = service[:tariff][:tariff_template][:field_templates]
+					service[:tariff][:tariff_template].delete(:field_templates)
+
+					@field_templates.each do |ft|
+						@values = ft[:values]
+						ft.delete(:values)
+						
+						@values.delete_if do |value|
+						value["tariff_id"] != service[:tariff]["id"]
+						end
 		
-		@places.each do |place|
-			@services = place[:services]
-			place.delete(:services)
-
-			@services.each do |service|
-				@field_templates = service[:tariff][:tariff_template][:field_templates]
-				service[:tariff][:tariff_template].delete(:field_templates)
-
-				@field_templates.each do |ft|
-					@values = ft[:values]
-					ft.delete(:values)
-					
-					@values.delete_if do |value|
-					value["tariff_id"] != service[:tariff]["id"]
+						ft[:value] = @values
 					end
-	
-					ft[:value] = @values
+					service[:tariff][:tariff_template][:field_templates] = @field_templates
 				end
-				service[:tariff][:tariff_template][:field_templates] = @field_templates
+				place[:service] = @services
 			end
-			place[:service] = @services
-		end
 
-		render json: @places
+			render json: @places
+		else
+			render json: {error: "user not found"}
 		end
 
 	def create
