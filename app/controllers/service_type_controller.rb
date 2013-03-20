1785ab2e727d963bd92ceaa6d605810711cf848f
lessthancold <lessthancold@gmail.com>
2013-03-20 13:39:27 +0400

    Bug_Fix: Fixed non-existant service types selection in service_types controller

--- app/controllers/service_type_controller.rb
+++ app/controllers/service_type_controller.rb
@@ -5,19 +5,11 @@ class ServiceTypeController < ApplicationController
 	end
 
 	def index_non_existant
-		@excluded_service_type_ids = Service.where(place_id: params[:place_id]).uniq.select(:service_type_id)
-		@services = Service.where(place_id: params[:place_id])
-		if @excluded_service_type_ids.blank?
-			if @services.blank?
-				@service_types = ServiceType.select("id, title").all
-				render json: { service_type: @service_types }
-			else
-				render json: { service_type: []}
-			end
-		else
-			@service_types = ServiceType.where("id NOT IN (?)", @excluded_service_type_ids.map(&:service_type_id).first).select("id, title")
-			render json: { service_type: @service_types }
-		end
+		@existant_service_type_ids = Service.where(place_id: params[:place_id]).uniq.select("service_type_id as id").map(&:id)
+		@service_type_ids = ServiceType.select(:id).map(&:id)
+		@non_existant_service_type_ids = @service_type_ids - @existant_service_type_ids
+		@service_types = ServiceType.where(id: @non_existant_service_type_ids).select("id, title")
+		render json: @service_types
 	end
 
 	def create
