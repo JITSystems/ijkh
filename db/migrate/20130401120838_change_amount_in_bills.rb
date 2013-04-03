f13dcdf7ba5b3c9f57bf251b41a7d27227dfc0e9
lessthancold <lessthancold@gmail.com>
2013-04-03 15:14:43 +0400

    large update

--- db/migrate/20130401120838_change_amount_in_bills.rb
+++ db/migrate/20130401120838_change_amount_in_bills.rb
@@ -1,9 +1,9 @@
 class ChangeAmountInBills < ActiveRecord::Migration
   def up
-  	change_column :bills, :amount, :decimal
+  	
   end
 
   def down
-  	change_column :bills, :amount, :string
+  	
   end
 end
--- db/migrate/20130401140433_change_amount_type_in_bills.rb
+++ db/migrate/20130401140433_change_amount_type_in_bills.rb
@@ -1,9 +1,9 @@
 class ChangeAmountTypeInBills < ActiveRecord::Migration
   def up
-  	change_column :bills, :amount, :string
+  	
   end
 
   def down
-  	change_column :bills, :amount, :decimal
+  	
   end
 end
