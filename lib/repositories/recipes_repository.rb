a971cb91434ddead0178031d24937ed47957172d
lessthancold <lessthancold@gmail.com>
2013-04-29 19:38:53 +0400

    Update

--- lib/repositories/recipes_repository.rb
+++ lib/repositories/recipes_repository.rb
@@ -3,18 +3,20 @@ module RecipesRepository
 	def create_recipe user, params
 		amount = params[:amount].to_f
 		if amount < 500.00
-		po_tax = 3
-		service_tax = 0.015*amount
-		total = po_tax + service_tax + amount
+			po_tax = 3
+			service_tax = (0.015*amount).round(2)
+			total = po_tax + service_tax + amount
 		else
-			service_tax = 0.03*amount
+			service_tax = (0.03*amount).round(2)
 			po_tax = 0
 			total = service_tax + amount
 		end
 		currency = "RUB"
 
+		amount = format_amount amount
+
 		recipe_params = {
-			amount: amount.to_s,
+			amount: amount,
 			service_id: params[:service_id],
 			account_id: params[:account_id],
 			user_id: user.id,
@@ -34,5 +36,17 @@ module RecipesRepository
 	def get_service_id recipe_id
 		recipe = Recipe.find(recipe_id)
 		return recipe.service_id
+	end
+	
+	private
+
+	def format_amount amount
+		amount = amount.to_s.split(".")
+		if amount.last =~ /\d\d/
+			amount_str = amount.first + "." + amount.last 
+		else
+			amount_str = amount.first + "." + amount.last + "0"
+		end
+		amount_str
 	end
 end
