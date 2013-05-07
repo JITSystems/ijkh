3dad1afe8219db38966a8d00f88174e35a8e31cd
lessthancold <lessthancold@gmail.com>
2013-05-07 18:10:33 +0400

    Update

--- app/models/analytic.rb
+++ app/models/analytic.rb
@@ -1,5 +1,5 @@
 class Analytic < ActiveRecord::Base
-	extend AnalyticRepository
+	extend AnalyticsRepository
 
   attr_accessible :amount, :place_id, :place_title, :service_id, :service_title, :tariff_title, :user_id
 
