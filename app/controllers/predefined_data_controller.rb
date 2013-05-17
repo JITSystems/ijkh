da529c52baac23ada45460802ab61eba923c6faa
jwsilent <mamonov.nick@gmail.com>
2013-05-17 13:36:50 +0400

    Pre-production commit

--- app/controllers/predefined_data_controller.rb
+++ app/controllers/predefined_data_controller.rb
@@ -1,5 +1,16 @@
 class PredefinedDataController < ApplicationController
 	def index
-		render json: {api: "1.0"} #dummy
+		if current_user
+			render json: {
+						user: {
+							auth_token: current_user.authentication_token, 
+                       		email: current_user.email,
+                       		first_name: current_user.first_name,
+                       		phone_number: current_user.phone_number
+                      			} 
+                    	}
+		else
+			render json: {error: "Auth failed"} #dummy
+		end
 	end
 end
