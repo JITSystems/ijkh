64cb33f9ef3eadac15cde7bd6c0737c0357545f4
jwsilent <mamonov.nick@gmail.com>
2013-05-18 19:37:31 +0400

    Require auth token before filter 8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
 			logger.info !params[:auth_token]
 			logger.info params[:auth_token] == ""
-			render json: {error: {message: "No auth token"}}, status: 401
+			render json: {error: {message: "No auth token", code: 401}}, status: 401
 		end
 
 	end
