de6b1f14da4ea08494169040fcb4396b015d5ee1
jwsilent <mamonov.nick@gmail.com>
2013-05-22 09:05:50 +0400

    web intarface in progress 60

--- app/controllers/web_interface/profile_controller.rb
+++ app/controllers/web_interface/profile_controller.rb
@@ -2,6 +2,11 @@ class WebInterface::ProfileController < WebInterfaceController
 	def show
 		@places = Place.where("user_id = ? and is_active = true", current_user.id).order("id DESC")
 		@place = @places.first
+		@place_id = ""
+		if @place
+			@place_id = @place.id
+		end 
+
 		@service_types = ServiceType.select("id, title").all
     	@vendors = Vendor.select("id, title, service_type_id").all
     	@tariff_templates = TariffTemplate.select("id, title, vendor_id, has_readings").where("vendor_id != 0")
