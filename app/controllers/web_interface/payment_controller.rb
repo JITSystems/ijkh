1ede2a01904797e0f12927f2016c762ba7420a75
jwsilent <mamonov.nick@gmail.com>
2013-05-22 12:50:30 +0400

    web intarface in progress 68

--- app/controllers/web_interface/payment_controller.rb
+++ app/controllers/web_interface/payment_controller.rb
@@ -1,10 +1,12 @@
 class WebInterface::PaymentController < WebInterfaceController
 	def show
 		@places = Place.where("user_id = ? and is_active = true", current_user.id).order("id DESC")
-		@place = @places.first
-		@services = @place.services.order("id DESC")
-		@service = @services.first
-		#@tariff = @service.tariff
+		if @places != []
+			@place = @places.first
+			@services = @place.services.order("id DESC")
+			@service = @services.first
+			#@tariff = @service.tariff
+		end
 	end
 
 	def get_payment_data
