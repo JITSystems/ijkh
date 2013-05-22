a035a8dd0bbb594d9b9040008add1abae5bbd414
jwsilent <mamonov.nick@gmail.com>
2013-05-22 09:13:23 +0400

    web intarface in progress 62

--- app/models/service_type.rb
+++ app/models/service_type.rb
@@ -4,6 +4,6 @@ class ServiceType < ActiveRecord::Base
   attr_accessible :title
 
   has_many :vendors, select: 'id, title, service_type_id, merchant_id, is_active'
-  has_many :tariff_templates, select: 'id, title, service_type_id'
+  has_many :tariff_templates, select: 'id, title, service_type_id, has_readings'
 
 end
