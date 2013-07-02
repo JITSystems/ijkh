3bd63d1598439f1079a54d90347dc659c9eff60a
jwsilent <mamonov.nick@gmail.com>
2013-07-02 17:21:51 +0400

    refactoring

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Ijkh
     config.autoload_paths += %W(#{config.root}/lib/repositories)
     config.autoload_paths += %W(#{config.root}/lib/analytics)
     config.autoload_paths += %W(#{config.root}/lib/object_manager)
+    config.autoload_paths += %W(#{config.root}/lib/params_manager)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
