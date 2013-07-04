5e8f036aeee1a20e994c4db24ea31084d46bfdd7
jwsilent <mamonov.nick@gmail.com>
2013-07-04 18:46:01 +0400

    apns

--- app/controllers/predefined_data_controller.rb
+++ app/controllers/predefined_data_controller.rb
@@ -20,7 +20,8 @@ class PredefinedDataController < ApplicationController
       APNS.pem  = 'apns.pem'
       APNS.port = Settings.apns.port.to_i
 	  device_token = 'ed582741 0ba963a1 72666ec2 2dc94ae1 69e1d184 deff5b9b f45214a2 d877833d'
-      APNS.send_notification(device_token, :alert => 'Privet, drug!', :sound => 'default')
+      APNS.send_notification(device_token, :alert => params[:text], :sound => 'default')
+
       render json: "success"
 	end
 end
