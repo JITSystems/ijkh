91665f59a82c2ae7dd931b0d5c57ca3a6ca926a4
jwsilent <mamonov.nick@gmail.com>
2013-07-05 15:44:02 +0400

    ios_device_token

---
+++ lib/clock.rb
@@ -0,0 +1,7 @@
+require File.expand_path('../../config/boot',        __FILE__)
+require File.expand_path('../../config/environment', __FILE__)
+require 'clockwork'
+
+include Clockwork
+
+every(30.seconds, 'zatrahat_dimu.job') { PushNotificationsWorker.test }
