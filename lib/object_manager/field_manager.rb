40ee3ab025c8677617190628e24df51c85417a98
jwsilent <mamonov.nick@gmail.com>
2013-07-11 14:35:54 +0400

    asd

--- lib/object_manager/field_manager.rb
+++ lib/object_manager/field_manager.rb
@@ -2,7 +2,6 @@ class FieldManager < ObjectManager
 	
 	def self.create(params, service, tariff, user)
 		params[:service][:tariff][:fields].each do |p|
-			puts p.inspect
 			field = Field.create!(FieldParams.create(p, tariff))
 			meter_reading = MeterReading.create!(MeterReadingParams.create(p, service, field, user)) if field.is_for_calc
 		end
