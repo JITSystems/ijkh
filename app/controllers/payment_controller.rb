7dd67f659fa2891312af60708c15515674022d29
jwsilent <mamonov.nick@gmail.com>
2013-07-24 16:36:07 +0400

    faye testing

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -12,7 +12,7 @@ class PaymentController < ApplicationController
 
 	def test
 		client = Faye::Client.new('http://ec2-54-245-202-30.us-west-2.compute.amazonaws.com:9292/faye')
-		client.publish("/#{params[:auth_token]}", {text: 'Yo nigga!'})
+		client.publish("/#{params[:auth_token]}", {text: "#{params[:text]}"})
 		render json: {thats: "Good"}
 	end
 end
