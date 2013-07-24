e9503e24fd771ca6f2aad5ab5e524d548455afb7
jwsilent <mamonov.nick@gmail.com>
2013-07-24 13:44:09 +0400

    faye testing

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -5,6 +5,6 @@ class PaymentController < ApplicationController
     		puts message.inspect
   			client.publish("/#{params[:auth_token]}", 'text' => 'Yo nigga!')
   		end
-  		render json: {status: "ok, now wait for async mess"}
+  		render json: {base_url: "http://ec2-54-245-202-30.us-west-2.compute.amazonaws.com:9292/faye", channel_title: "#{params[:auth_token]}"}
 	end
 end
