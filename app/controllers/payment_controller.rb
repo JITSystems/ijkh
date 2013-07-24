80d688b4dec2c29cc1988808d77e2ab258d4002b
jwsilent <mamonov.nick@gmail.com>
2013-07-24 13:56:42 +0400

    faye testing

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -11,9 +11,8 @@ class PaymentController < ApplicationController
 	def pay
 		client = Faye::Client.new('http://localhost:9292/faye')
 		client.subscribe("/#{params[:auth_token]}") do |message|
-    		puts message.inspect
   			client.publish("/#{params[:auth_token]}", 'text' => 'Yo nigga!')
   		end
-  		render json: {base_url: "http://ec2-54-245-202-30.us-west-2.compute.amazonaws.com:9292/faye", channel_title: "/#{params[:auth_token]}"}
+  		render json: {}
 	end
 end
