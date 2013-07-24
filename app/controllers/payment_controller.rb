68edc4b4bf217cf5cd0bf34ddc6b116e3b00cd6c
jwsilent <mamonov.nick@gmail.com>
2013-07-24 12:55:32 +0400

    Faye

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -3,7 +3,7 @@ class PaymentController < ApplicationController
 		client = Faye::Client.new('http://localhost:9292/faye')
 		client.subscribe("/#{params[:auth_token]}") do |message|
     		puts message.inspect
-  			client.publish('/server', 'text' => 'Oppa!!')
+  			client.publish("/#{params[:auth_token]}", 'text' => 'Yo nigga!')
   		end
 	end
 end
