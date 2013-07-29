ac360874f32c4b86cad72152f0f0e22ca688e01a
jwsilent <mamonov.nick@gmail.com>
2013-07-29 17:13:22 +0400

    faye+sidekiq

--- app/workers/http_request_worker.rb
+++ app/workers/http_request_worker.rb
@@ -18,18 +18,18 @@ class HttpRequestWorker
 			if response["transaction"]["result"] == "Error"
 				case response["transaction"]["errorCode"]
 				when "1"
-					publish_message = {result: "failure", message: "При оплате вашего платежа сервисом Pay Online возникла техническая ошибка. Пожалуйста, повторите попытку позже."}
+					publish_message = {"result" => "failure", "message" => "При оплате вашего платежа сервисом Pay Online возникла техническая ошибка. Пожалуйста, повторите попытку позже."}
 				when "2"
-					publish_message = {result: "failure", message: "Платеж по вашей карте отклонен. Воспользуйтесь другой картой."}
+					publish_message = {"result" => "failure", "message" => "Платеж по вашей карте отклонен. Воспользуйтесь другой картой."}
 				when "3"
-					publish_message = {result: "failure", message: "Платеж по вашей карте отклонен банком-эмитентом карты. Свяжитесь с вашим банком или воспользуйтесь другой картой и повторите запрос."}
+					publish_message = {"result" => "failure", "message" => "Платеж по вашей карте отклонен банком-эмитентом карты. Свяжитесь с вашим банком или воспользуйтесь другой картой и повторите запрос."}
 				when "4"
 					# 3ds
 				else
-					publish_message = {result: "failure", message: "При оплате счета произошла неизвестная ошибка."}
+					publish_message = {"result" => "failure", "message" => "При оплате счета произошла неизвестная ошибка."}
 				end
 			else
-				publish_message = {result: "success", message: "Платеж был успешно проведен. Данные поступили в обработку."}
+				publish_message = {"result" => "success", "message" => "Платеж был успешно проведен. Данные поступили в обработку."}
 			end
 		end
 				
