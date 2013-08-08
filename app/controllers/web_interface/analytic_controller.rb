2908413fa320fcd1d7d8ae77f6f1c248ea9a86c1
nocool <john.loudless@gmail.com>
2013-08-08 22:53:01 +0400

    analytic fix

--- app/controllers/web_interface/analytic_controller.rb
+++ app/controllers/web_interface/analytic_controller.rb
@@ -6,10 +6,6 @@ class WebInterface::AnalyticController < WebInterfaceController
 		@month_analytics = MonthAnalytic.get_annual current_user
 
 		#@detailed_payments = DetailedPayment.get_by_service_id(current_user, params[:service_id], params[:month])
-		@csv_string = CSV.generate do |csv|
-    		csv << ["row", "of", "CSV", "data"]
-    		csv << ["another", "row"]
-    		# ...
-  		end
+
 	end
 end
