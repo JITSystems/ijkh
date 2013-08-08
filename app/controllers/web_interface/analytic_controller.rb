06e083ccc5d8946b261f4348be1cddc0d23d0e94
john.loudless@gmail.com <nocool@ps.(none)>
2013-08-08 20:52:25 +0400

    month analytic

--- app/controllers/web_interface/analytic_controller.rb
+++ app/controllers/web_interface/analytic_controller.rb
@@ -1,6 +1,15 @@
 class WebInterface::AnalyticController < WebInterfaceController
+	#require 'csv'
 	def show
-			@payment_histories = PaymentHistory.all
-		
+		@payment_histories = PaymentHistory.all
+
+		@month_analytics = MonthAnalytic.get_annual current_user
+
+		#@detailed_payments = DetailedPayment.get_by_service_id(current_user, params[:service_id], params[:month])
+		@csv_string = CSV.generate do |csv|
+    		csv << ["row", "of", "CSV", "data"]
+    		csv << ["another", "row"]
+    		# ...
+  		end
 	end
 end
