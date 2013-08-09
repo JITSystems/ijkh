ce6b19c419a17f8b58646468a1ebe6b95cd25dcb
jwsilent <mamonov.nick@gmail.com>
2013-08-09 22:11:47 +0400

    fixed poll statistics

--- lib/poll_statistics.rb
+++ lib/poll_statistics.rb
@@ -6,15 +6,15 @@ class PollStatistics
 		poll_questions.each do |pq|
 			answers = []
 			statistics_el[:quiz_question] = pq.as_json
+			custom_answers = pq.quiz_results.select("custom_answer").where("custom_answer != ''").as_json
+				if custom_answers.first
+					statistics_el[:quiz_question].merge!(custom_answers: custom_answers)
+				end
 			poll_answers = pq.quiz_answers
 			poll_answers.each do |pa|
 				answer = pa.as_json
 				answers_total = pa.quiz_results.count
 				answer[:quiz_result] = {count: answers_total}
-				custom_answers = pq.quiz_results.select("custom_answer").where("custom_answer != ''").as_json
-				if custom_answers.first
-					answer.merge!(custom_answers: custom_answers)
-				end
 				answers << answer
 			end
 			statistics_el[:quiz_question][:quiz_answer] = answers
