836c91760000414cce67ff63dfc095daa4978313
jwsilent <mamonov.nick@gmail.com>
2013-08-16 11:48:16 +0400

    Notifications

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,11 +61,8 @@ Ijkh::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.middleware.use ExceptionNotifier,
-                        sender_address: 'no-reply@izkh.ru',
-                        exception_recipients: 'development@izkh.ru'
-
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.middleware.use ExceptionNotifier, sender_address: 'no-reply@izkh.ru', exception_recipients: 'development@izkh.ru'
 end
