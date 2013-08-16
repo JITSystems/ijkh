0ecad8adf60fc4a30d4545b59a81bcad639e5fa2
jwsilent <mamonov.nick@gmail.com>
2013-08-16 12:11:57 +0400

    Notifications

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,5 +64,5 @@ Ijkh::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.middleware.use ExceptionNotifier, sender_address: 'no-reply@izkh.ru', exception_recipients: 'development@izkh.ru'
+  config.middleware.use ExceptionNotification::Notifier, sender_address: 'no-reply@izkh.ru', exception_recipients: 'development@izkh.ru'
 end
