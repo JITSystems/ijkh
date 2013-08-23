36a617ca987720fa7941bba91cd79836be698cef
jwsilent <mamonov.nick@gmail.com>
2013-08-23 15:53:08 +0400

    fuck up w/non utility

--- app/controllers/non_utility_vendor_controller.rb
+++ app/controllers/non_utility_vendor_controller.rb
@@ -8,9 +8,9 @@ class NonUtilityVendorController < ApplicationController
 	def create
 		@non_utility_vendor = NonUtilityVendor.create!(params[:non_utility_vendor])
 		if params[:picture]
-			map_name = params[:title]
+			map_name = @non_utility_vendor.id
 		  	name = map_name.to_s+'.png'
-		  	directory = File.join('public','images','non_utility_maps', params[:non_utility_vendor][:non_utility_service_type_id].to_s)
+		  	directory = File.join('public','images','non_utility_maps', @non_utility_vendor.non_utility_service_type_id.to_s)
 
 		  	unless File.directory?(directory)
 		  	  FileUtils.mkdir_p(directory)
@@ -18,7 +18,7 @@ class NonUtilityVendorController < ApplicationController
 
 		  	path = File.join(directory, name)
 		  	File.open(path, "wb") { |f| f.write(open(params[:picture][:url]).read) }
-		  	directory = File.join('images','non_utility_maps', params[:non_utility_vendor][:non_utility_service_type_id].to_s)
+		  	directory = File.join('images','non_utility_maps', @non_utility_vendor.non_utility_service_type_id.to_s)
 		  	path = File.join(directory, name)
 		  	path
 		  	@non_utility_vendor.update_attribute(:picture_url, path)
