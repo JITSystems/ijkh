f5679c9eb976d7de29588aab1fb238818fb6caed
jwsilent <mamonov.nick@gmail.com>
2013-09-06 18:19:03 +0400

    Push

--- app/workers/push_notifications_worker.rb
+++ app/workers/push_notifications_worker.rb
@@ -6,14 +6,15 @@ class PushNotificationsWorker
     APNS.pem  = Settings.apns.pem_file
     APNS.port = Settings.apns.port.to_i
     
-    users = User.all
-    users.each do |user|
+    #users = User.all
+    #users.each do |user|
+    user = User.find(2)
         if user.ios_device_status == :active
             APNS.send_notification(user.ios_device_token, :alert => text, :sound => 'default')
         else
             raise "iOS device is inactive"
         end
-    end
+    #end
   end
 
 end
