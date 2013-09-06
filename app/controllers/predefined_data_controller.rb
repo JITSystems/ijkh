2417114695358ed023c519d0f70eb2253e42a0dd
jwsilent <mamonov.nick@gmail.com>
2013-09-06 18:40:16 +0400

    Push

--- app/controllers/predefined_data_controller.rb
+++ app/controllers/predefined_data_controller.rb
@@ -23,7 +23,16 @@ class PredefinedDataController < ApplicationController
  
 
 	def apns
-	  PushNotificationsWorker.perform_async("В связи с техническими работами временно недоступна оплата через мобильное приложение 'АйЖКХ'. Оплату услуг Вы можете осуществить на сайте сервиса izkh.ru. Приносим извинения за неудобства.")
+    APNS.host = Settings.apns.host
+    APNS.pem  = Settings.apns.pem_file
+    APNS.port = Settings.apns.port.to_i
+    text = "В связи с техническими работами временно недоступна оплата через мобильное приложение 'АйЖКХ'. Оплату услуг Вы можете осуществить на сайте сервиса izkh.ru. Приносим извинения за неудобства."
+    #users = User.all
+    #users.each do |user|
+    user = User.find(2)
+    if user.ios_device_token
+      APNS.send_notification(user.ios_device_token, :alert => text, :sound => 'default')
+    end
     render json: {status: "success"}
 	end
 end
