0a1e762b1b9b13bbcb1aac0ad1e86f459db3c835
jwsilent <mamonov.nick@gmail.com>
2013-09-09 14:21:40 +0400

    Push

--- app/workers/push_notifications_worker.rb
+++ app/workers/push_notifications_worker.rb
@@ -6,19 +6,13 @@ class PushNotificationsWorker
     APNS.host = Settings.apns.host
     APNS.pem  = Settings.apns.pem_file
     APNS.port = Settings.apns.port.to_i
-    
-    #users = User.all
-    #users.each do |user|
     text = "Оплата через приложение восстановлена!"
-    user = User.find(2)
-        if user.ios_device_token
-            APNS.send_notification(user.ios_device_token, :alert => text, :sound => 'default')
-        end
-    user = User.find(1)
-        if user.ios_device_token
-            APNS.send_notification(user.ios_device_token, :alert => text, :sound => 'default')
-        end
-    #end
+    users = User.all
+    users.each do |user|
+      if user.ios_device_token
+        APNS.send_notification(user.ios_device_token, :alert => text, :sound => 'default')
+      end
+    end
   end
 
 end
