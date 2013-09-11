b7e57c73ebbeb5431e7c5f6ee5d5d80f906ac5bc
jwsilent <mamonov.nick@gmail.com>
2013-09-11 15:03:45 +0400

    apns

--- app/workers/push_notifications_worker.rb
+++ app/workers/push_notifications_worker.rb
@@ -8,13 +8,13 @@ class PushNotificationsWorker
     APNS.port = Settings.apns.port.to_i
     text = 'Оплата временно недоступна.'
     #text = text.encode("unicode")
-    #users = User.all
-    #users.each do |user|
-    user = User.find(2)
+    users = User.all
+    users.each do |user|
+    #user = User.find(2)
       if user.ios_device_token
         apn = APNS.send_notification(user.ios_device_token, {:alert => text, :sound => 'default'})
       end
-    #end
+    end
     logger.info text
   end
 
