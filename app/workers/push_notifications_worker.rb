7075ab4ac94990f8e112376783e5cacd6d3da79d
jwsilent <mamonov.nick@gmail.com>
2013-09-11 12:38:41 +0400

    apns

--- app/workers/push_notifications_worker.rb
+++ app/workers/push_notifications_worker.rb
@@ -6,16 +6,16 @@ class PushNotificationsWorker
     APNS.host = Settings.apns.host
     APNS.pem  = Settings.apns.pem_file
     APNS.port = Settings.apns.port.to_i
-    text = 'Что то длиннее чем 80 символов, и чуть длиннее'
+    text = 'Что то длиннее чем 80 символов, и чуть длиннее'.as_json
     #text = text.encode("unicode")
     #users = User.all
     #users.each do |user|
     user = User.find(2)
       if user.ios_device_token
         apn = APNS.send_notification(user.ios_device_token, {:alert => text, :sound => 'default'})
-        logger.info apn.inspect
       end
     #end
+    logger.info text
   end
 
 end
