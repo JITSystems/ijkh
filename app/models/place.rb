631f6cce18296dcb2a2354139ffa489e5c760220
jwsilent <mamonov.nick@gmail.com>
2013-09-24 17:32:42 +0400

    place fix

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,7 +6,7 @@ class Place < ActiveRecord::Base
   validates_presence_of :building, :city, :street, :title, :user_id
 
   belongs_to :user, foreign_key: :user_id
-  belongs_to :city, foreign_key: :city_id
+  #belongs_to :city, foreign_key: :city_id
 
   has_many :services, conditions: "is_active = true"
   has_many :analytics
