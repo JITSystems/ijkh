e0476d6b2b0b3cadfc80a0e7d75a566fcad4ca34
jwsilent <mamonov.nick@gmail.com>
2013-10-04 11:21:39 +0400

    amount format fix

--- lib/commission_manager.rb
+++ lib/commission_manager.rb
@@ -3,7 +3,7 @@ class CommissionManager
   def calculate(amount, commission=0.0)
   	commission_hash = {}
   	amount = FloatModifier.modify(amount)
-  	commission_hash[:total] = FloatModifier.modify(amount + amount*(commission/100.00)))
+  	commission_hash[:total] = FloatModifier.modify(amount + amount*(commission/100.00))
   	commission_hash[:service_tax] = FloatModifier.modify(amount*(commission/100.00))
   	commission_hash[:po_tax] = 0.0
 
