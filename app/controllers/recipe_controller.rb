b90c3431f1b9084150534d2c5a87b76163022b96
jwsilent <mamonov.nick@gmail.com>
2013-10-21 15:18:27 +0400

    commission fix

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -2,7 +2,7 @@ class RecipeController < ApplicationController
 	def create
 		@recipe = RecipeManager.new
 		@recipe = @recipe.create(ServiceManager.get(params[:service_id]), params[:amount])
-		render 'recipe/show'
+		render json: @recipe
 	end
 
 	def show_last
