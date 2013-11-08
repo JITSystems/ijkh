e93afc48b8b3e0c0c582660ef12fab41209ab9a9
codenameisone <codename.is.one@gmail.com>
2013-11-08 13:12:37 +0400

    osmp

--- app/workers/jt_integration_worker.rb
+++ app/workers/jt_integration_worker.rb
@@ -3,10 +3,10 @@ class JtIntegrationWorker
 	include Sidekiq::Worker
 
 	def perform(user_id, user_account)
-		amount = Osmp.check(user_id, user_account, DateTime.now.to_s(:number))
+		amount = Osmp.check(user_account, DateTime.now.to_s(:number))
 		service = Service.where("user_account = ? and vendor_id = 16 and user_id = ?", user_account, user_id).first
 		account = service.account
-		if amount && amount.to_f > 0
+		if amount && amount.to_f > 0.0
 			account.update_attributes!(amount: amount, status: -1)
 		end
 	end
