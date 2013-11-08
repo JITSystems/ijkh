628495b85cdd2b5b5c358f2c38d79f7a8f138b75
codenameisone <codename.is.one@gmail.com>
2013-11-08 14:36:31 +0400

    osmp

--- app/workers/jt_integration_worker.rb
+++ app/workers/jt_integration_worker.rb
@@ -6,6 +6,8 @@ class JtIntegrationWorker
 		services.each do |service|
 			user_account = service.user_account
 			amount = Osmp.check(user_account, DateTime.now.to_s(:number))
+			if amount.to_f < 0.0
+				amount = (amount.to_s)[1..-1]
 			account = service.account
 			if amount && amount.to_f > 0.0
 				account.update_attributes!(amount: amount, status: -1)
