11460407744a33b8c7273586f581c5b5d3e44fbd
codenameisone <codename.is.one@gmail.com>
2013-11-08 14:09:18 +0400

    osmp

--- lib/osmp.rb
+++ lib/osmp.rb
@@ -1,8 +1,8 @@
 class Osmp
 	def self.check(user_account, date)
 		user_account = user_account.to_s
-	unless user_account.to_s.length <= 7
-		(7 - user_account.to_s.length).times { user_account.insert!(0, '0') }
+	unless user_account.to_s.length >= 7
+		(7 - user_account.to_s.length).times { user_account.insert(0, '0') }
 	end
 
 	uri = URI.parse("https://193.33.144.3:65443/bgbilling/mpsexecuter/13/5?command=check&txn_id=11441119&account=2%23#{user_account}&txn_date=#{date}&sum=1.00")
