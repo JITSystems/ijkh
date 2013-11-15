7be29c74f6ec414904ed694bc62baee5f706a6ca
codenameisone <codename.is.one@gmail.com>
2013-11-15 10:45:27 +0400

    ovd

--- lib/ovd.rb
+++ lib/ovd.rb
@@ -1,122 +1,47 @@
 #!/usr/bin/ruby -w
 #encoding: utf-8
-require 'nokogiri'
-require "base64"
-
 class Ovd
+	def self.xls_parser
+		s = Roo::Excel.new("ovd_data/precinct.xls")
+		address, houses = [], []
+		(1..s.last_row).each do |i|
 
-	def self.xml_parser
-		Dir.foreach('ovd_data') do |file|
-			next if file == '.' or file == '..'
-			xmlfile = File.new("ovd_data/#{file}")
-			xml_doc = Nokogiri::XML(xmlfile)
-			data = []
+			address_ovd = s.cell(i, 2).split(", ")
+			full_name 	= 	s.cell(i, 4).split(" ")
+			sector	 	= 	s.cell(i, 5).split("; ")
+			sector.each { |ad| address = ad.split(",") }
 
-			(0..xml_doc.css("EFOType OVD opor_info opor").size-1).each do |k|
-				(0..xml_doc.css("EFOType OVD opor_info opor")[k].children.css("uum_info uum").size-1).each do |i|
+			Precinct.create!(
+				ovd: 				s.cell(i, 1),
+				ovd_town: 			address_ovd[0],
+				ovd_street: 		address_ovd[1],
+				ovd_house: 			address_ovd[2],
+				ovd_telnumber: 		s.cell(i, 3) ? s.cell(i, 3).to_i : s.cell(i, 3),
+				surname: 			full_name[0],
+				name: 				full_name[1],
+				middlename: 		full_name[2]
+			)
+			PrecinctStreet.create!(street: address[0].lstrip) unless PrecinctStreet.where(street: address[0]).first
 
-					unless xml_doc.css("EFOType OVD opor_info opor")[k].children.css("uum_info uum uum_surname")[i].text == "Вакантно" || xml_doc.css("EFOType OVD opor_info opor")[k].children.css("uum_info uum uum_surname")[i].text == "Вакансия" || xml_doc.css("EFOType OVD opor_info opor")[k].children.css("uum_info uum uum_surname")[i].text == "вакантный"
-						# Create precincts
-						unless Precinct.where(surname: xml_doc.css("EFOType OVD opor_info opor")[k].children.css("uum_info uum uum_surname")[i].text).first				
-							Precinct.create!(
-								ovd: xml_doc.css("EFOType OVD opor_info opor subdiv_name")[k].text,
-								ovd_town: xml_doc.css("EFOType OVD opor_info opor opor_townname")[k].text,
-								ovd_street: xml_doc.css("EFOType OVD opor_info opor opor_streetname")[k].text,
-								ovd_house: xml_doc.css("EFOType OVD opor_info opor opor_house")[k].text,
-								ovd_telnumber: xml_doc.css("EFOType OVD opor_info opor opor_telnumber")[k].text,
-								surname: xml_doc.css("EFOType OVD opor_info opor")[k].children.css("uum_info uum uum_surname")[i].text,
-								name: xml_doc.css("EFOType OVD opor_info opor")[k].children.css("uum_info uum uum_name")[i].text,
-								middlename: xml_doc.css("EFOType OVD opor_info opor")[k].children.css("uum_info uum uum_middlename")[i].text
-							)
-						end
-						# Post request to create precincts on service
-						# PostRequest.precinct(
-						# 	xml_doc.css("EFOType OVD opor_info opor subdiv_name")[k].text,
-						# 	xml_doc.css("EFOType OVD opor_info opor opor_townname")[k].text,
-						#   	xml_doc.css("EFOType OVD opor_info opor opor_streetname")[k].text,
-						#    	xml_doc.css("EFOType OVD opor_info opor opor_house")[k].text, 
-						#    	xml_doc.css("EFOType OVD opor_info opor opor_telnumber")[k].text,
-						#     xml_doc.css("EFOType OVD opor_info opor")[k].children.css("uum_info uum uum_surname")[i].text,
-						#     xml_doc.css("EFOType OVD opor_info opor")[k].children.css("uum_info uum uum_name")[i].text,
-						#     xml_doc.css("EFOType OVD opor_info opor")[k].children.css("uum_info uum uum_middlename")[i].text
-						# )
+			(1..address.size).each do |i|
+				next if address[i] == nil || address[i] == " "
+				houses = []
+				houses << case address[i].gsub(' ', '')
+						  when /(\d+)-(\d+)/
+							array = []
+							address[i].gsub(' ', '') =~ /(\d+)-(\d+)/
+							array << ($1..$2).to_a.each { |x| x.to_i }
+							array.flatten!.uniq	
+						  else
+							address[i]
+						  end
 
-						(0..xml_doc.css("EFOType OVD opor_info opor")[k].children.css("uum_info uum")[i].children.css("territory_info uumterritory").size-1).each do |j|
-
-							#Create streets
-							unless PrecinctStreet.where(street: xml_doc.css("EFOType OVD opor_info opor")[k].children.css("uum_info uum")[i].children.css("territory_info uumterritory uum_streetname")[j].text).first
-								PrecinctStreet.create!(
-									street: xml_doc.css("EFOType OVD opor_info opor")[k].children.css("uum_info uum")[i].children.css("territory_info uumterritory uum_streetname")[j].text
-								)
-							end
-
-							houses = xml_doc.css("EFOType OVD opor_info opor")[k].children.css("uum_info uum")[i].children.css("territory_info uumterritory houses")[j].text
-							houses.gsub!('=', '-')
-							houses.gsub!('.', ',')
-							houses.gsub!(';', ',')
-							houses.gsub!(" ", "")
-							houses.gsub!("а", "")
-
-							# Parsing address
-							data = case houses
-								   when "все дома" || "полностью" || "все" then ""
-								   when /(\d+)-(\d+),(\d+)-(\d+),(\d+)-(\d+),(\d+)-(\d+)/
-										array = []
-										array << houses.gsub('-', ',').gsub('/', ',').split(',')
-										houses =~ /(\d+)-(\d+),(\d+)-(\d+),(\d+)-(\d+),(\d+)-(\d+)/
-										array << ($1..$2).to_a.each { |x| x.to_i }
-										array << ($3..$4).to_a.each { |x| x.to_i }
-										array << ($5..$6).to_a.each { |x| x.to_i }
-										array.flatten!.uniq
-								   when /(\d+)-(\d+),(\d+)-(\d+)/
-										array = []
-										array << houses.gsub('-', ',').gsub('/', ',').split(',')
-										houses =~ /(\d+)-(\d+),(\d+)-(\d+)/
-										array << ($1..$2).to_a.each { |x| x.to_i }
-										array << ($3..$4).to_a.each { |x| x.to_i }
-										array.flatten!.uniq
-								   when /(\d+)-(\d+)/
-										array = []
-										array << houses.gsub('-', ',').gsub('/', ',').split(',')
-										houses =~ /(\d+)-(\d+)/
-										array << ($1..$2).to_a.each { |x| x.to_i }
-										array.flatten!.uniq									
-								   when /[\w][,]/
-										array = []
-										array = houses.split(',')
-								   when /с(\d+)до(\d+),с(\d+)до(\d+)/
-										array = []
-										houses =~ /с(\d+)до(\d+),с(\d+)до(\d+)/
-										array << ($1..$2).to_a.each { |x| x.to_i }
-										array << ($3..$4).to_a.each { |x| x.to_i }
-										array.flatten!.uniq
-									when /с(\d+)до(\d+)/
-										array = []
-										houses =~ /с(\d+)до(\d+)/
-										array << ($1..$2).to_a.each { |x| x.to_i }
-								 		array.flatten!.uniq
-									when /с(\d+)до(\d+)/
-										array = []
-										houses =~ /с(\d+)до(\d+)/
-										array << ($1..$2).to_a.each { |x| x.to_i }
-								 		array.flatten!.uniq
-									when /(\d+)/
-										array = []
-										array << houses
-									else ""
-									end
-							#Create territories of precinct 
-							(0..data.size-1).each do |d|
-								PrecinctHouse.create!(
-									precinct_id: Precinct.where(surname: xml_doc.css("EFOType OVD opor_info opor")[k].children.css("uum_info uum uum_surname")[i].text).first.id,
-									precinct_street_id: PrecinctStreet.where(street: xml_doc.css("EFOType OVD opor_info opor")[k].children.css("uum_info uum")[i].children.css("territory_info uumterritory uum_streetname")[j].text).first.id,
-							    	house: data[d]
-								)
-								#Post request to create precinct's territories on service
-								# PostRequest.precinct_territory(n+1, xml_doc.css("EFOType OVD opor_info opor")[k].children.css("uum_info uum")[i].children.css("territory_info uumterritory uum_streetname")[j].text, data[d])
-							end	
-						end
-				    end
+				houses.flatten.each do |house|
+				PrecinctHouse.create!(
+					precinct_id: 		Precinct.where(surname: full_name[0], name: full_name[1]).first.id,
+					precinct_street_id: PrecinctStreet.where(street: address[0]).first.id,
+			    	house: 				house.lstrip
+				)
 				end
 			end
 		end
