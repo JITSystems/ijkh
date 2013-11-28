4bd9b4d4aa2179fa3d041e728453b6d88c462ff8
codenameisone <codename.is.one@gmail.com>
2013-11-28 13:00:12 +0400

    gt integration

--- app/workers/gt_integration_worker.rb
+++ app/workers/gt_integration_worker.rb
@@ -10,7 +10,7 @@ class GtIntegrationWorker
 			gt = GlobalTelecom.new(user_account)
 			amount = gt.check
 
-			amount = (amount.to_s)[1..-1] if amount.to_f < 0.0
+			amount = amount.to_s
 			account = service.account
 			account.update_attributes!(amount: amount, status: -1) if amount
 		end
