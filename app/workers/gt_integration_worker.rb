17329cf3cd72a7b09493ca8b082ca39c73cd5a6f
codenameisone <codename.is.one@gmail.com>
2013-11-28 12:44:12 +0400

    gt integration

--- app/workers/gt_integration_worker.rb
+++ app/workers/gt_integration_worker.rb
@@ -3,7 +3,7 @@ class GtIntegrationWorker
 	include Sidekiq::Worker
 
 	def perform(user_id)
-		services = Service.where("user_id = ? and vendor_id = 16 and is_active = true", user_id)
+		services = Service.where("user_id = ? and vendor_id = 121 and is_active = true", user_id)
 		services.each do |service|
 			user_account = service.user_account
 
