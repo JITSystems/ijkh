74d7e6b478e01e95e5bf68611a12cf466c40402b
codenameisone <codename.is.one@gmail.com>
2013-12-19 12:49:05 +0400

    notifiers

--- lib/pay_online_response_manager.rb
+++ lib/pay_online_response_manager.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class PayOnlineResponseManager
 	def self.publish_message(response)
 		if response["transaction"]
--- lib/tds_response_manager.rb
+++ lib/tds_response_manager.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class TdsResponseManager
 
 	def self.publish_message(response)
