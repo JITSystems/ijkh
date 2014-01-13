e2f7d47d1adae416bad10882e515ae93fdf86792
nocool <john.loudless@gmail.com>
2014-01-13 12:03:13 +0400

    freelance interface in progress

--- app/controllers/freelance_interface/freelancers_controller.rb
+++ app/controllers/freelance_interface/freelancers_controller.rb
@@ -4,9 +4,20 @@ class FreelanceInterface::FreelancersController < FreelanceInterfaceController
 
 	skip_before_filter :require_current_user
 
+	before_filter :check_existence, :only => :new
+
+	def check_existence
+		@freelancer = FreelanceInterface::Freelancer.where(user_id: current_user.id).first || nil
+		if @freelancer
+			render 'show', id: @freelancer.id
+		else
+			render 'new'
+		end
+	end
+
+
 	def show
 		@freelancer = FreelanceInterface::Freelancer.new
-
 	end
 
 	
@@ -19,19 +30,26 @@ class FreelanceInterface::FreelancersController < FreelanceInterfaceController
 		tags.each do |tag|
 			@tags_array << [tag.title, tag.id]
 		end 
-
 		
 		# FAKE DATA
 		# FreelanceInterface::Tag.delete_all
 		# ['Уборка','Ремонт','Телевизоров','Обучение','Седовник','Водопроводчик','Монтажник','Разнорабочий','Уборка','Ремонт','Няня','web design','Ремонт стиральных машин','Ubuntu','twitter','Маляр','wordpress','youtube','Электрик','web 2.0','motion design','work','телефонмастер','турникмен','игра на гитаре','катание на сноуборде'].each do |tag|
 		# 	FreelanceInterface::Tag.create!(title: tag, published: true)
 		# end
-
 	end
 
 	
 	def create
-		@freelancer = FreelanceInterface::Freelancer.create(params[:freelance_interface_freelancer], current_user)
+		freelancer_params = {
+			name: params[:freelance_interface_freelancer][:name],
+ 			surname: params[:freelance_interface_freelancer][:surname],
+ 			phone_number: params[:freelance_interface_freelancer][:phone_number],
+ 			published: false,
+			unpublish_at: Date.current() + params[:freelance_interface_freelancer][:unpublish_at].to_i.month,
+			user_id: current_user.id
+		}
+
+		@freelancer = FreelanceInterface::Freelancer.create(freelancer_params)
 
 		if @freelancer.save 
 			render 'show', id: @freelancer.id
@@ -56,8 +74,8 @@ class FreelanceInterface::FreelancersController < FreelanceInterfaceController
 	  end
 	end
 
-	def index
 
+	def index
 		@freelancers = FreelanceInterface::Freelancer.all
 	end
 
