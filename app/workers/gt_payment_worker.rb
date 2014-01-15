401699a937d1fe98a7eab410f703f42f4f8e3fc8
codenameisone <codename.is.one@gmail.com>
2014-01-15 13:48:35 +0400

    integration fixes

--- app/workers/gt_payment_worker.rb
+++ app/workers/gt_payment_worker.rb
@@ -3,7 +3,7 @@ class GtPaymentWorker
 	include Sidekiq::Worker
 
 	def perform(service_id, recipe_id, amount)
-		services = Service.find(service_id)
+		service = Service.find(service_id)
 		user_account = service.user_account
 		gt = GlobalTelecom.new(user_account, recipe_id, amount)
 		amount = gt.pay
--- app/workers/sl_payment_worker.rb
+++ app/workers/sl_payment_worker.rb
@@ -3,7 +3,7 @@ class SlPaymentWorker
 	include Sidekiq::Worker
 
 	def perform(service_id, recipe_id, amount)
-		services = Service.find(service_id)
+		service = Service.find(service_id)
 		user_account = service.user_account
 		sl = SamaraLan.new(user_account, recipe_id, amount)
 		sl.pay
