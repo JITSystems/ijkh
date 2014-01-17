4fb68e0f0b8a47044a75f8bfc76d4945342c73d9
nocool <john.loudless@gmail.com>
2014-01-17 11:29:38 +0400

    datepicker on news creation page

--- app/controllers/freelance_interface/moderation_controller.rb
+++ app/controllers/freelance_interface/moderation_controller.rb
@@ -2,5 +2,7 @@ class FreelanceInterface::ModerationController < FreelanceInterfaceController
 
 	def show
 		@freelancers = FreelanceInterface::Freelancer.where(published: false)
+		@tags = FreelanceInterface::Tag.where(published: false)
+		@comments = FreelanceInterface::Comment.where(published: false)
 	end
 end
