96c3a55878065ec15e46d7be055f2a12bd5d13d7
codenameisone <codename.is.one@gmail.com>
2014-01-27 16:22:30 +0400

    Error fix cities

--- lib/object_manager/non_utility_vendor_manager.rb
+++ lib/object_manager/non_utility_vendor_manager.rb
@@ -15,7 +15,7 @@ class NonUtilityVendorManager < ObjectManager
 		  	File.open(path, "wb") { |f| f.write(open(params[:picture][:url]).read) }
 		  	directory = File.join('/','home','ubuntu','apps','shared','images','non_utility_maps', non_utility_vendor.non_utility_service_type_id.to_s)
 		  	path = File.join(directory, name)
-		  	path = "images/non_utility_maps/#{@non_utility_vendor.non_utility_service_type_id}/#{name}"
+		  	path = "images/non_utility_maps/#{non_utility_vendor.non_utility_service_type_id}/#{name}"
 		  	non_utility_vendor.update_attribute(:picture_url, path)
 		end
 		return non_utility_vendor
