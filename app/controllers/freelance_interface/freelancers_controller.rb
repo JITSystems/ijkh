6c8946218e69b05a16741a2b2516dab0ed31217b
nocool <john.loudless@gmail.com>
2014-01-29 00:12:15 +0400

    freelance interface in progress

--- app/controllers/freelance_interface/freelancers_controller.rb
+++ app/controllers/freelance_interface/freelancers_controller.rb
@@ -35,7 +35,7 @@ class FreelanceInterface::FreelancersController < FreelanceInterfaceController
 			@tags_array << [tag.title, tag.id]
 		end 
 		
-		# заполнение тестовыми тегами
+		# # заполнение тестовыми тегами
 		# FreelanceInterface::Tag.delete_all
 		# ['Уборка','Ремонт','Телевизоров','Обучение','Седовник','Водопроводчик','Монтажник','Разнорабочий','Уборка','Ремонт','Няня','web design','Ремонт стиральных машин','Ubuntu','twitter','Маляр','wordpress','youtube','Электрик','web 2.0','motion design','work','телефонмастер','турникмен','игра на гитаре','катание на сноуборде'].each do |tag|
 		# 	FreelanceInterface::Tag.create!(title: tag, published: true)
