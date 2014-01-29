a9b7f87d08e3daea0d097ca8e4ab84b042ca3de5
codenameisone <codename.is.one@gmail.com>
2014-01-29 16:35:13 +0400

    craft

--- lib/craft_s.rb
+++ lib/craft_s.rb
@@ -1,9 +1,10 @@
 # encoding: utf-8
 class CraftS
 	require 'uri'
-	def initialize(user_account, date, amount=nil, order_id=nil)
+	def initialize(user_account, date, account_type, amount=nil, order_id=nil)
 		@user_account = user_account
 		@date = date
+		@account_type = account_type
 		@amount = amount
 		@order_id = order_id
 		@url = "https://billing.kraft-s.ru:47035/pays/izkh.php"
@@ -24,11 +25,11 @@ class CraftS
 protected
 
 	def form_check_url
-		"#{@url}?ID=#{DateTime.now.to_s(:number)}&DATE=#{@date}&TYPE=4&ATYPE=inet&ACCOUNT=#{@user_account.to_s}&SUM=0"
+		"#{@url}?ID=#{DateTime.now.to_s(:number)}&DATE=#{@date}&TYPE=4&ATYPE=#{account_type}&ACCOUNT=#{@user_account.to_s}&SUM=0"
 	end
 
 	def form_pay_url
-		"#{@url}?ID=#{@order_id}&DATE=#{@date}&TYPE=1&ACCOUNT=#{@user_account.to_s}&ATYPE=inet&SUM=#{@amount.to_s}"
+		"#{@url}?ID=#{@order_id}&DATE=#{@date}&TYPE=1&ACCOUNT=#{@user_account.to_s}&ATYPE=#{account_type}&SUM=#{@amount.to_s}"
 	end
 	
 	def get_response(response)
