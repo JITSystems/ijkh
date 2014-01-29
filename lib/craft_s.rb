e9d806e806d7a4dd504abcb7f8e9b839774ac6c5
codenameisone <codename.is.one@gmail.com>
2014-01-29 11:42:15 +0400

    craft s integration

--- lib/craft_s.rb
+++ lib/craft_s.rb
@@ -22,11 +22,11 @@ class CraftS
 protected
 
 	def form_check_url
-		"#{@url}?ID=#{DateTime.now.to_s(:number)}&DATE=#{@date}&TYPE=4&ACCOUNT=#{@user_account}&SUM=0"
+		"#{@url}?ID=#{DateTime.now.to_s(:number)}&DATE=#{@date}&TYPE=4&ACCOUNT=#{@user_account.to_s}"
 	end
 
 	def form_pay_url
-		"#{@url}?ID=#{order_id}&DATE=#{@date}&TYPE=1&ACCOUNT=#{@user_account}&SUM=#{@amount}"
+		"#{@url}?ID=#{order_id}&DATE=#{@date}&TYPE=1&ACCOUNT=#{@user_account.to_s}&SUM=#{@amount}"
 	end
 	
 	def get_response(response)
