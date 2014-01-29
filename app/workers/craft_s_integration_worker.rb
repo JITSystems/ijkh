a5c445e07085bbd2f513494a5dbe48758c2eee49
codenameisone <codename.is.one@gmail.com>
2014-01-29 17:51:53 +0400

    craft

--- app/workers/craft_s_integration_worker.rb
+++ app/workers/craft_s_integration_worker.rb
@@ -8,33 +8,35 @@ class CraftSIntegrationWorker
 		services.each do |service|
 			user_account = service.user_account
 
-			tariff_template_id = service.tariff.tariff_template_id
-			case tariff_template_id.to_i
-			when 153
-				account_type = "inet"
-			when 158
-				account_type = "voip"
-			else
-				account_type = nil
-				raise "No account type"
-			end
+			if service.tariff_id
+				tariff_template_id = service.tariff.tariff_template_id
+				case tariff_template_id.to_i
+				when 153
+					account_type = "inet"
+				when 158
+					account_type = "voip"
+				else
+					account_type = nil
+					raise "No account type"
+				end
 
-			cs = CraftS.new(user_account, DateTime.now.strftime("%Y-%m-%d %H:%M:%S"), account_type)
-			cs_check = cs.check
-			if cs_check
-				if cs_check[:debt]
-					amount = cs.check[:debt]
+				cs = CraftS.new(user_account, DateTime.now.strftime("%Y-%m-%d %H:%M:%S"), account_type)
+				cs_check = cs.check
+				if cs_check
+					if cs_check[:debt]
+						amount = cs.check[:debt]
+					else
+						amount = 0.0
+					end
 				else
 					amount = 0.0
 				end
-			else
-				amount = 0.0
-			end
 
-			if amount.to_f < 0.0
-				amount = (amount.to_s)[1..-1] if amount.to_f < 0.0
-				account = service.account
-				account.update_attributes!(amount: amount, status: -1) if amount
+				if amount.to_f < 0.0
+					amount = (amount.to_s)[1..-1] if amount.to_f < 0.0
+					account = service.account
+					account.update_attributes!(amount: amount, status: -1) if amount
+				end
 			end
 		end
 	end
--- app/workers/jt_integration_worker.rb
+++ app/workers/jt_integration_worker.rb
@@ -7,27 +7,29 @@ class JtIntegrationWorker
 		services = Service.where('user_id = ? and vendor_id = 16 and is_active = true', user_id)
 		services.each do |service|
 			user_account = service.user_account
-			tariff_template_id = service.tariff.tariff_template_id
+			if service.tariff_id
+				tariff_template_id = service.tariff.tariff_template_id
 
-			case tariff_template_id.to_i
-			when 157
-				prefix = "1"
-			when 155
-				prefix = "2"
-			when 156
-				prefix = "3"
-			else
-				prefix = nil
-				raise "No prefix"
-			end
+				case tariff_template_id.to_i
+				when 157
+					prefix = "1"
+				when 155
+					prefix = "2"
+				when 156
+					prefix = "3"
+				else
+					prefix = nil
+					raise "No prefix"
+				end
 
-			osmp = Osmp.new(user_account, DateTime.now.to_s(:number), prefix)
-			amount = osmp.check
+				osmp = Osmp.new(user_account, DateTime.now.to_s(:number), prefix)
+				amount = osmp.check
 
-			if amount.to_f < 0.0
-				amount = (amount.to_s)[1..-1] if amount.to_f < 0.0
-				account = service.account
-				account.update_attributes!(amount: amount, status: -1) if amount
+				if amount.to_f < 0.0
+					amount = (amount.to_s)[1..-1] if amount.to_f < 0.0
+					account = service.account
+					account.update_attributes!(amount: amount, status: -1) if amount
+				end
 			end
 		end
 	end
