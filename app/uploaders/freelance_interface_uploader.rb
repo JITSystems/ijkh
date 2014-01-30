4264e00756322c2bc97f3e68a6f88ae9029d7191
nocool <john.loudless@gmail.com>
2014-01-30 11:55:51 +0400

    fi carrierwave configure

--- app/uploaders/freelance_interface_uploader.rb
+++ app/uploaders/freelance_interface_uploader.rb
@@ -3,6 +3,8 @@
 class FreelanceInterfaceUploader < CarrierWave::Uploader::Base
 
  include CarrierWave::MiniMagick
+
+ storage :fog
  
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,7 +14,8 @@ CarrierWave.configure do |config|
     :provider              => 'AWS',
     :aws_access_key_id     => ENV['S3_KEY'],
     :aws_secret_access_key => ENV['S3_SECRET'],
-    :region                => ENV['S3_REGION']
+    :region                => ENV['S3_REGION'],
+    :host                  => ENV['S3_ASSET_URL']
   }
  
   # For testing, upload files to local `tmp` folder.
@@ -29,6 +30,6 @@ CarrierWave.configure do |config|
   # config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
  
   config.fog_directory    = ENV['S3_BUCKET_NAME']
-  config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
-  config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
+  config.fog_public     = true    
+  # config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
 end
