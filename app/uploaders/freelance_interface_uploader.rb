d4df0f3fb90abb39fd4ceff6d5571ce83812d256
nocool <john.loudless@gmail.com>
2014-01-30 12:00:59 +0400

    fi carrierwave configure

--- app/uploaders/freelance_interface_uploader.rb
+++ app/uploaders/freelance_interface_uploader.rb
@@ -10,17 +10,17 @@ class FreelanceInterfaceUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
  
-  version :print do
-    version :thumb    { process :resize_to_fit => [32, 32] }
-    version :preview  { process :resize_to_fit => [256, 256] }
-    version :full     { process :resize_to_fit => [2048, 2048] }
-  end
+  # version :print do
+  #   version :thumb    { process :resize_to_fit => [32, 32] }
+  #   version :preview  { process :resize_to_fit => [256, 256] }
+  #   version :full     { process :resize_to_fit => [2048, 2048] }
+  # end
  
-  version :web do
-    version :thumb    { process :resize_to_fit => [32, 32] }
-    version :preview  { process :resize_to_fit => [128, 128] }
-    version :full     { process :resize_to_fit => [1024, 768] }
-  end
+  # version :web do
+  #   version :thumb    { process :resize_to_fit => [32, 32] }
+  #   version :preview  { process :resize_to_fit => [128, 128] }
+  #   version :full     { process :resize_to_fit => [1024, 768] }
+  # end
  
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
