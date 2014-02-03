535ca357477d85f27d5ba1264e9e26f8f0478333
codenameisone <codename.is.one@gmail.com>
2014-02-03 12:59:03 +0400

    Fix user service

--- lib/params_manager/service_params.rb
+++ lib/params_manager/service_params.rb
@@ -8,14 +8,15 @@ class ServiceParams < ParamsManager
 private
 	
 	def self.service_params(params, user)
-		
 		s_p = {
 						  place_id: 		params[:place_id],
 						  service_type_id: 	params[:service_type_id],
-						  vendor_id: 		params[:vendor][:id],
 						  user_id: 			user.id,
 						  is_active: 		true
 						 }
+		
+		params[:vendor] ? s_p.merge!(vendor_id: params[:vendor][:id]) : s_p.merge!(vendor_id: 0)
+
 		unless s_p[:vendor_id].to_s != "0" || params[:vendor_id]
 			service_type = ServiceTypeManager.get(params[:service_type_id])
 			s_p.merge!(title: service_type.title + " (Пользовательский)")
