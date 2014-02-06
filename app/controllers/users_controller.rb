80b717c315ddcd0750650d675734be9e7bfc4913
codenameisone <codename.is.one@gmail.com>
2014-02-06 22:54:16 +0400

    users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
 	def index
-		@users = User.select_all
+		@users = ActiveRecord::Base.connection.select_values('SELECT first_name, email, created_at, sign_in_count, phone_number, ios_device_token, last_sign_in_ip, last_sign_in_at FROM users')
 		render json: @users
 	end
 
