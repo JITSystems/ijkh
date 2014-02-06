f7334476d8bbc15cdfa68e4dc7703358086921d7
codenameisone <codename.is.one@gmail.com>
2014-02-06 17:23:36 +0400

    users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
 	def index
-		@users = User.all
+		@users = User.select([:first_name, :email, :created_at, :sign_in_count, :phone_number, :ios_device_token, :last_sign_in_ip, :last_sign_in_at]).all
 		render json: @users
 	end
 
