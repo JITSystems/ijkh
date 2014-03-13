501599a3479a7ae56d02d31b454575db9a542367
Nikita Mamonov <mamonov.nick@gmail.com>
2014-03-13 12:45:04 +0400

    service sub title

--- app/models/utility_metric.rb
+++ app/models/utility_metric.rb
@@ -1,6 +1,9 @@
 class UtilityMetric < ActiveRecord::Base
   attr_accessible :energy_phase_common, :energy_phase_one, :energy_phase_two, :gas, :user_id, :vendor_id, :water_cold, :water_hot, :processed
 
+
+  has_one :utility_metric_setting
+  
   belongs_to :user
   belongs_to :vendor
 end
