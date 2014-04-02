cfabe686d4ea61cc5fc7612de9043c59ba70d98b
Nikita Mamonov <mamonov.nick@gmail.com>
2014-04-02 11:20:18 +0400

    terminal fix

--- app/models/terminal_payment.rb
+++ app/models/terminal_payment.rb
@@ -1,3 +1,4 @@
 class TerminalPayment < ActiveRecord::Base
   attr_accessible :amount, :commission, :tariff_template_id, :total, :user_account, :vendor_id
+  validates_presence_of :amount, :commission, :tariff_template_id, :total, :user_account, :vendor_id
 end
