0ad6b9dd329c62ce4cf6efbed2583997559a6fa0
ivannasya <vagrant@vagrant.(none)>
2014-04-10 13:29:03 +0000

    test yandex

--- config/routes.rb
+++ config/routes.rb
@@ -189,14 +189,14 @@ get 'api/1.0/users' => 'users#index'
 
 #Yandex money
   # paymentAvisoURL
-  get 'api/1.0/payment_notify/test' => 'yandex_money#notify'
+  post 'api/1.0/payment_notify/test' => 'yandex_money#notify'
   # checkURL
-  get 'api/1.0/payment_check/test' => 'yandex_money#notify'
+  post 'api/1.0/payment_check/test' => 'yandex_money#notify'
   # successURL
   get 'api/1.0/payment_success/test' => 'web_interface/payment#show'
   # failURL
   get 'api/1.0/payment_fail/test' => 'web_interface/payment#show'
-  
+
 # Web Interface
   scope '/' do
       get 'analytic' => 'web_interface/analytic#show'
