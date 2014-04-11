6fbbf2626d64e6bc5dcdcdcdb1c6919bff139290
Nikita Mamonov <mamonov.nick@gmail.com>
2014-04-12 01:00:55 +0400

    fix

--- app/controllers/yandex_money_controller.rb
+++ app/controllers/yandex_money_controller.rb
@@ -7,7 +7,7 @@ class YandexMoneyController < ApplicationController
   def check
     @check = YandexMoney.new(params[:requestDatetime], params[:md5], params[:orderSumCurrencyPaycash], params[:orderSumBankPaycash], params[:orderNumber], params[:customerNumber], params[:orderSumAmount], params[:invoiceId]).check
     logger.info @check
-    render xml: "yandex_money/check"
+    render :template => "yandex_money/check.xml.erb", :layout => false 
   end
 
   def notify
