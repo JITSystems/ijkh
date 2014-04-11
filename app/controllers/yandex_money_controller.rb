19f2385a14fe054881592f219a8e4e78d6e022dc
Nikita Mamonov <mamonov.nick@gmail.com>
2014-04-12 00:53:06 +0400

    fix

--- app/controllers/yandex_money_controller.rb
+++ app/controllers/yandex_money_controller.rb
@@ -1,5 +1,8 @@
 class YandexMoneyController < ApplicationController
   skip_before_filter :require_auth_token
+  after_filter do
+    logger.info response.body
+  end
   
   def check
     @check = YandexMoney.new(params[:requestDatetime], params[:md5], params[:orderSumCurrencyPaycash], params[:orderSumBankPaycash], params[:orderNumber], params[:customerNumber], params[:orderSumAmount], params[:invoiceId]).check
