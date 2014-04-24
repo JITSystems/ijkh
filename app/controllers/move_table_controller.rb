323ebf15af65056c83dd54b5c4dd3a32ab959696
ivannasya <vagrant@vagrant.(none)>
2014-04-24 10:01:10 +0000

    scripts for move db to iz-revival

--- app/controllers/move_table_controller.rb
+++ app/controllers/move_table_controller.rb
@@ -1,7 +1,6 @@
 class MoveTableController < ApplicationController
 
   def move
-    data = MoveTable.new("http://192.168.0.100/users", "User").move
-    render json: data
+    render json: MoveTable.new(params[:table]).move.to_json
   end
 end
--- lib/move_table.rb
+++ lib/move_table.rb
@@ -1,30 +1,15 @@
 # encoding: utf-8
 class MoveTable
 
-  def initialize(url, from_table)
-    @url = url
+  def initialize(from_table)
     @from_table = from_table
   end
 
-  def auth_token 
-    response = HTTParty.post( "#{@url}/users/sign_in.json",
-      :body => { :user =>  { :email => "test@iz.ru", :password => "qwerty" }}.to_json,
-      :headers => { 'Content-Type' => 'application/json' })
-    response.parsed_response["user"]["auth_token"]
-  end
-
   def move
-    # line_data, data, lenth = {}, {}, 1
-    # attrs = @from_table.constantize.column_names
-    # @from_table.constantize.all.each do |line|
-    #   attrs.each {|a| line_data[a] = line.send(a)}
-    #   data[lenth] = line_data
-    #   lenth += 1
-    # end
-    response = HTTParty.post( "http://192.168.0.100/users?auth_token=LuNXcS4tAGMgj8xwr7LR",
-      :body => { test: {lol: "lollol"}}.to_json,
-      :headers => {'Content-Type' => 'application/json'})
+    data = {}
+    attrs = @from_table.constantize.column_names
+    @from_table.constantize.all.each do |line|
+      attrs.each {|a| data[a] = line.send(a)}
+    end
   end
 end
-
-      # :body => { data: data, metadata: { lenth: lenth }}.to_json,
